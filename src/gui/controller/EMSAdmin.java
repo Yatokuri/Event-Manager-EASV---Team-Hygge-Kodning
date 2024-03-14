a83ba2da6416dea4bd6338b565e0c4a70b6a7c1f
Naylin <naylinoohla@gmail.com>
2024-03-14 14:40:33 +0100

    Updated CSS + Updated Admin GUI + Added User Icon

--- src/gui/controller/EMSAdmin.java
+++ src/gui/controller/EMSAdmin.java
@@ -1,4 +1,15 @@
 package gui.controller;
 
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+
 public class EMSAdmin {
+    public HBox eventHBox;
+
+    public void eventList()
+    {
+        HBox eventHBox = new HBox();
+        eventHBox.setAlignment(Pos.CENTER);
+
+    }
 }
