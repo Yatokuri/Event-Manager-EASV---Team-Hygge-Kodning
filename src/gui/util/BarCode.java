867bf1af39b351d5455332a8ecf47fcefed5b28a
Yatokuri <144425585+Yatokuri@users.noreply.github.com>
2024-04-04 12:39:10 +0200

    Update BarCode.java

--- src/gui/util/BarCode.java
+++ src/gui/util/BarCode.java
@@ -5,12 +5,19 @@ import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import java.awt.image.BufferedImage;
 import java.util.Map;
 
 public class BarCode {
+
+    public static BufferedImage generateEAN13BarcodeImage(String barcodeText, int width, int height) {
+        EAN13Writer barcodeWriter = new EAN13Writer();
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.EAN_13, width, height);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
     public static BufferedImage generateQRCodeImage(String data, Map<EncodeHintType, ErrorCorrectionLevel> hashmap, int width, int height) throws Exception{
         String charset = "UTF-8";
         hashmap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
